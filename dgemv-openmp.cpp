b266b6ccbf9601390d15b9b9263200630f24fe2e
benztimm <ebuddharuksa@sfsu.edu>
2023-10-01 19:02:36 -0700

    add openmp code

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <omp.h>
 
-const char* dgemv_desc = "OpenMP dgemv.";
+const char *dgemv_desc = "OpenMP dgemv.";
 
 /*
  * This routine performs a dgemv operation
@@ -12,26 +12,29 @@ const char* dgemv_desc = "OpenMP dgemv.";
  * On exit, A and X maintain their input values.
  */
 
-void my_dgemv(int n, double* A, double* x, double* y) {
-
-   #pragma omp parallel
-   {
-      int nthreads = omp_get_num_threads();
-      int thread_id = omp_get_thread_num();
-      printf("my_dgemv(): Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
-      printf("my_dgemv(): For actual timing runs, please comment out these printf() and omp_get_*() statements. \n");
-   }
-
-   // insert your dgemv code here. you may need to create additional parallel regions,
-   // and you will want to comment out the above parallel code block that prints out
-   // nthreads and thread_id so as to not taint your timings
-   #pragma omp parallel for 
-      for (int i = 0; i < n; i++) {
-         #pragma omp parallel for reduction (+:y[i]) 
-            for (int j = 0; j < n; j++) {
-               y[i] += A[i * n + j] * x[j];
-            }
-      }
-
+void my_dgemv(int n, double *A, double *x, double *y)
+{
+/*
+#pragma omp parallel
+{
+   int nthreads = omp_get_num_threads();
+   int thread_id = omp_get_thread_num();
+   printf("my_dgemv(): Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
+   printf("my_dgemv(): For actual timing runs, please comment out these printf() and omp_get_*() statements. \n");
 }
-
+*/
+// insert your dgemv code here. you may need to create additional parallel regions,
+// and you will want to comment out the above parallel code block that prints out
+// nthreads and thread_id so as to not taint your timings
+   #pragma omp parallel for
+   for (int i = 0; i < n; i++)
+   {
+      double sum = 0.0;
+      #pragma omp simd reduction(+ : sum)
+      for (int j = 0; j < n; j++)
+      {
+         sum += A[i * n + j] * x[j];
+      }
+      y[i] += sum;
+   }
+}
